6c9bd3ed227951f79c6bff9916f43c92f32f4cd8
avros16 <114757828+avros16@users.noreply.github.com>
2024-03-18 12:37:52 +0000

    examples a to c

--- .ccls-cache/@home@runner@midterm-example-questions/main.cpp
+++ .ccls-cache/@home@runner@midterm-example-questions/main.cpp
@@ -1,5 +1,51 @@
 #include <iostream>
 
-int main() {
-  std::cout << "Hello World!\n";
+int main()
+{
+
+
+using std::cout;
+using std::cin;
+using std::endl;
+    
+//     int x{10};
+//      while (x>0) {
+//          cout << x << endl;
+//          x=x+3;
+//      }
+
+   // *********************** example B ********************************
+
+  // int main() {
+
+  //  int n{1};
+
+  //  while(n<=20){
+  //      cout << n << endl;
+  //      n++;
+  //  }
+  // }
+
+
+  // ***************************** exmaple c **********************************
+
+float temp{0};
+
+  cout << "Please enter your body temperature: " << endl;
+  cin >> temp;
+
+  if (temp <= 98.6) {
+      cout << "You are okay to go to school\n";
+  } else if (temp >= 98.6 && temp <=100.1) {
+      cout << "You might be coming down with something, drink alot of fluids\n";
+  } else if (temp > 100.1) {
+      cout << "Omg you are not feeling well!\n";
+  }
+
+
+  
+
+  
+
+  
 }
