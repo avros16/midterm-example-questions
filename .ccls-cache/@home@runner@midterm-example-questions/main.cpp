8d0ff6e19cb9b51ccd5fce01e0bbedb9e9bb1ac1
avros16 <114757828+avros16@users.noreply.github.com>
2024-03-20 13:04:33 +0000

    last pp problem

--- .ccls-cache/@home@runner@midterm-example-questions/main.cpp
+++ .ccls-cache/@home@runner@midterm-example-questions/main.cpp
@@ -207,4 +207,21 @@ using std::endl;
 // }
 
 
+ // ********* exmaple 2 from power point *****************
+
+  // #include <iostream>
+  // using namespace std;
+  // int main(){
+  // // declare and initialize variables
+  // int number = 0, i;
+  // // prompt user to input
+  // cout << "Please enter an integer: ";
+  // cin >> number;
+  // // enter loop
+  // for (i = 1; i < number; i *= 2){
+  // cout << i << " ";
+  // }
+  // cout << i;
+  // }
+
 }
